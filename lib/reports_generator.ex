419e6620c549b6790b0cfee6412594b033097553
WilliamJagerDEV <williamjager.dev@gmail.com>
2023-03-03 16:30:41 -0300

    reading file

--- lib/reports_generator.ex
+++ lib/reports_generator.ex
@@ -1,18 +1,10 @@
 defmodule ReportsGenerator do
-  @moduledoc """
-  Documentation for `ReportsGenerator`.
-  """
 
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> ReportsGenerator.hello()
-      :world
-
-  """
-  def hello do
-    :world
+  # ReportsGenerator.build("report_test.csv")
+  def build(filename) do
+    case File.read("reports/#{filename}") do
+      {:ok, result} ->  result
+      {:error, reason} -> reason
+    end
   end
 end
